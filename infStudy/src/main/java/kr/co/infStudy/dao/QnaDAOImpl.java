44b6b82c22365df4a756859de485da80da17f77e
sakku-dev <kws1044@gmail.com>
2020-05-14 15:20:29 +0900

    test

--- infStudy/src/main/java/kr/co/infStudy/dao/QnaDAOImpl.java
+++ infStudy/src/main/java/kr/co/infStudy/dao/QnaDAOImpl.java
@@ -1,8 +1,13 @@
 package kr.co.infStudy.dao;
 
+import java.util.HashMap;
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import kr.co.infStudy.dto.qna.QnaDTO;
 
 @Repository
 public class QnaDAOImpl implements QnaDAO {
@@ -10,4 +15,15 @@ public class QnaDAOImpl implements QnaDAO {
 	@Autowired
 	private SqlSession session;
 	private String nameSpace = "kr.co.infStudy.qnaMapper.";
+	
+	@Override
+	public QnaDTO getQnaDetail(String q_no) {
+		return session.selectOne("tempName");
+	}
+
+	@Override
+	public List<QnaDTO> getQnaList(HashMap<String, String> hmap) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
